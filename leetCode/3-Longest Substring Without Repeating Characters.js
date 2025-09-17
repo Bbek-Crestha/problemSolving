eabfe847a24578ea55c573d997a8a943c65932dd
Bbek Crestha <bcrestha07@gmail.com>
2025-09-17 14:51:46 +0800

    solve 3 from leetcode

---
+++ leetCode/3-Longest Substring Without Repeating Characters.js
@@ -0,0 +1,22 @@
+var lengthOfLongestSubstring = function (s) {
+	let x = 0;
+	let str = '';
+
+	for (i = 0; i < s.length; i++) {
+		if (!str.includes(s[i])) {
+			str += s[i];
+		} else {
+			x = Math.max(str.length, x);
+			str = str.substring(str.indexOf(s[i]) + 1, i) + s[i];
+		}
+	}
+
+	return Math.max(x, str.length);
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
+console.log(lengthOfLongestSubstring('bbbbb'));
+console.log(lengthOfLongestSubstring('pwwkew'));
+
+// x = 'abcde';
+// console.log(x.substring(x.indexOf('b'), 2));
